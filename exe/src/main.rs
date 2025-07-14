8a2951534bf7c21a980df6a5b2009334765a0932
DJSdev <26153156+DJSdev@users.noreply.github.com>
2025-07-14 00:37:14 -0400

    clean up

--- exe/src/main.rs
+++ exe/src/main.rs
@@ -7,27 +7,22 @@ use windows::{
         System::{
             Diagnostics::Debug::{ReadProcessMemory, WriteProcessMemory},
             LibraryLoader::{GetProcAddress, LoadLibraryA},
-            Memory::{
-                VirtualProtect, PAGE_EXECUTE_READWRITE,
-                PAGE_PROTECTION_FLAGS,
-            },
+            Memory::{VirtualProtect, PAGE_EXECUTE_READWRITE, PAGE_PROTECTION_FLAGS},
             Threading::GetCurrentProcess,
         },
         UI::WindowsAndMessaging::{MessageBoxA, MESSAGEBOX_STYLE},
     },
 };
 
+static N_SIZE: usize = 13;
+
 fn main() {
     let lib_name = s!("user32.dll");
     let h_lib = unsafe { LoadLibraryA(lib_name).expect("Could not get library") };
 
     let func_to_hook = s!("MessageBoxA");
-
-    // Cast as c_void so we can easily increment the pointer
     let func_addr = unsafe { GetProcAddress(h_lib, func_to_hook).expect("Could not find function") }
         as *const c_void;
-
-    const N_SIZE: usize = 13; // jmp instructions are fixed to 64 bits wide
 
     // Save the protection flags for the region
     let mut original_protect: PAGE_PROTECTION_FLAGS = PAGE_PROTECTION_FLAGS::default();
@@ -41,11 +36,11 @@ fn main() {
         .expect("Unable to change access permissions to memory")
     };
 
-    // Save the first 16 bytes of the function
-    // We need 16 bytes in order to fit a jmp and our relative offset in it's place
+    // Save the first 13 bytes of the function
+    // We need 13 bytes in order to fit a jmp and our relative offset in it's place
     let mut saved_buffer: [u8; N_SIZE] = [0; N_SIZE];
-    let mut num_bytes_read: usize = 0;
 
+    let mut num_bytes_read: usize = 0;
     unsafe {
         ReadProcessMemory(
             GetCurrentProcess(),
@@ -59,12 +54,13 @@ fn main() {
 
     // mov r10, addr
     // jmp r10
+    //
     // The 0x00's will be populated with the memory address of message_box_a_proxy_func
     let mut relay_function: [u8; N_SIZE] = [
         0x49, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x41, 0xFF, 0xE2,
     ];
 
-    // This DLL holds our proxy function
+    // This DLL holds the proxy function
     let dll_module = unsafe { LoadLibraryA(s!("rs_test_dll.dll")).expect("Cannot open module") };
 
     let proc_addr = unsafe {
@@ -75,7 +71,6 @@ fn main() {
 
     // Place the address of the proxy function into the relay_function
     relay_function[2..10].copy_from_slice(&proc_addr.to_ne_bytes());
-
     println!("Relay function: {relay_function:x?}");
 
     unsafe {
@@ -89,9 +84,8 @@ fn main() {
         .expect("Could not write memory")
     };
 
-    let mut backup_protect: PAGE_PROTECTION_FLAGS = PAGE_PROTECTION_FLAGS::default();
     unsafe {
-        VirtualProtect(func_addr, N_SIZE, original_protect, &mut backup_protect)
+        VirtualProtect(func_addr, N_SIZE, original_protect, &mut PAGE_PROTECTION_FLAGS::default())
             .expect("Unable to change access permissions to memory back")
     };
 
