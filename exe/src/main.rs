002f0759ba658d913d0993712c3f929cdab10925
DJSdev <26153156+DJSdev@users.noreply.github.com>
2025-08-12 21:06:23 -0400

    wip: trampoline

--- exe/src/main.rs
+++ exe/src/main.rs
@@ -1,7 +1,7 @@
 // Based on work from https://kylehalladay.com/blog/2020/11/13/Hooking-By-Example.html
 mod util;
 
-use crate::util::find_func_addr;
+use crate::util::{find_func_addr, relocate_prologue_to_trampoline};
 use std::ffi::c_void;
 use windows::{
     core::s,
@@ -15,7 +15,8 @@ use windows::{
     },
 };
 
-static N_SIZE: usize = 13;
+static MIN_SIZE: usize = 14;
+static TRAMPOLINE_SIZE: usize = MIN_SIZE + 16;
 
 fn main() {
     let func_addr = find_func_addr("user32.dll", "MessageBoxA").unwrap_or_else(|err| {
@@ -23,21 +24,30 @@ fn main() {
         std::process::exit(1);
     });
 
+    println!("Original Function Address: {:x?}", func_addr);
+
+    println!("Next RIP-Relative Address: {:x?}", unsafe {
+        func_addr.add(MIN_SIZE)
+    });
+
     // Save the protection flags for the region
     let mut original_protect = PAGE_PROTECTION_FLAGS::default();
     unsafe {
         VirtualProtect(
             func_addr,
-            N_SIZE,
+            MIN_SIZE,
             PAGE_EXECUTE_READWRITE,
             &mut original_protect,
         )
         .expect("Unable to change access permissions to memory")
     };
 
-    // Save the first 13 bytes of the function
-    // We need 13 bytes in order to fit a jmp and our relative offset in it's place
-    let mut saved_buffer: [u8; N_SIZE] = [0; N_SIZE];
+    // Save the first 14 bytes of the function
+    // MessageBoxA starts with the following bytes, so we should see these in the `saved_buffer`
+    // .text:0000000180078B70 48 83 EC 38            sub     rsp, 38h
+    // .text:0000000180078B74 45 33 DB               xor     r11d, r11d
+    // .text:0000000180078B77 44 39 1D 92 87 03 00   cmp     cs:?gfEMIEnable@@3HA, r11d ; int gfEMIEnable
+    let mut saved_buffer: [u8; MIN_SIZE] = [0; MIN_SIZE];
 
     let mut num_bytes_read: usize = 0;
     unsafe {
@@ -45,19 +55,35 @@ fn main() {
             GetCurrentProcess(),
             func_addr,
             saved_buffer.as_mut_ptr() as *mut c_void,
-            N_SIZE,
+            MIN_SIZE,
             Some(&mut num_bytes_read),
         )
         .expect("Unable to read process memory");
     }
 
+    println!("Saved buffer: \n  {:x?}", saved_buffer);
+
+    // The 0x00's will be populated with the memory address of message_box_a_proxy_func
     // mov r10, addr
     // jmp r10
-    //
-    // The 0x00's will be populated with the memory address of message_box_a_proxy_func
-    let mut relay_function: [u8; N_SIZE] = [
-        0x49, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x41, 0xFF, 0xE2,
+    let mut new_func_prologue: [u8; MIN_SIZE] = [
+        0x49, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x41, 0xFF, 0xE2, 0x00, 
     ];
+
+    // When we overwrite the function prologue, use NOPs to ensure we don't have invalid instructions
+    let nop_func_prologue: [u8; MIN_SIZE] = [0x90; MIN_SIZE];
+
+    let mut trampoline = Vec::new();
+
+    let _ = relocate_prologue_to_trampoline(
+        saved_buffer.as_ptr(),
+        trampoline.as_mut_ptr(),
+        MIN_SIZE,
+    )
+    .expect("Uh oh, stinky");
+
+    println!("Trampoline: \n  {trampoline:x?}");
 
     let proc_addr =
         find_func_addr("rs_test_dll.dll", "message_box_a_proxy_func").unwrap_or_else(|err| {
@@ -65,18 +91,18 @@ fn main() {
             std::process::exit(1);
         }) as *const () as usize;
 
-    println!("Proxy Address: {proc_addr:x}");
+    println!("Proxy Address: \n  {proc_addr:x}");
 
-    // Place the address of the proxy function into the relay_function
-    relay_function[2..10].copy_from_slice(&proc_addr.to_ne_bytes());
-    println!("Relay function: {relay_function:x?}");
+    // Place the address of the proxy function into the new_func_prologue
+    new_func_prologue[2..10].copy_from_slice(&proc_addr.to_ne_bytes());
+    println!("Relay function: \n  {new_func_prologue:x?}");
 
     unsafe {
         WriteProcessMemory(
             GetCurrentProcess(),
             func_addr,
-            relay_function.as_ptr() as *const c_void,
-            N_SIZE,
+            new_func_prologue.as_ptr() as *const c_void,
+            MIN_SIZE,
             None,
         )
         .expect("Could not write memory")
@@ -85,7 +111,7 @@ fn main() {
     unsafe {
         VirtualProtect(
             func_addr,
-            N_SIZE,
+            MIN_SIZE,
             original_protect,
             &mut PAGE_PROTECTION_FLAGS::default(),
         )
--- exe/src/util.rs
+++ exe/src/util.rs
@@ -1,8 +1,20 @@
 use std::ffi::c_void;
 
+use iced_x86::{
+    BlockEncoder, BlockEncoderOptions, Code, Decoder, DecoderOptions, FlowControl, Formatter,
+    Instruction, InstructionBlock, NasmFormatter, Register, IntelFormatter
+};
 use windows::{
     core::PCSTR,
-    Win32::System::LibraryLoader::{GetProcAddress, LoadLibraryA},
+    Win32::System::{
+        Diagnostics::Debug::FlushInstructionCache,
+        LibraryLoader::{GetProcAddress, LoadLibraryA},
+        Memory::{
+            VirtualAlloc, VirtualProtect, MEM_COMMIT, MEM_RELEASE, MEM_RESERVE, PAGE_EXECUTE_READ,
+            PAGE_PROTECTION_FLAGS, PAGE_READWRITE,
+        },
+        Threading::GetCurrentProcess,
+    },
 };
 
 #[derive(Debug)]
@@ -40,10 +52,14 @@ impl ToPcstr for &str {
 ///
 /// `func_addr` - `*const c_void`
 pub fn find_func_addr(lib_name: &str, func_name: &str) -> Result<*const c_void, HookError> {
-    let lib_name = lib_name.into_pcstr();
-
-    let Ok(h_lib) = (unsafe { LoadLibraryA(lib_name) }) else {
-        return Err(HookError::new("Could not get library"));
+    let h_lib = match unsafe { LoadLibraryA(lib_name.into_pcstr()) } {
+        Err(err) => {
+            return Err(HookError::new(format!(
+                "Could not get library {:?} {:?}",
+                lib_name, err
+            )))
+        }
+        Ok(val) => val,
     };
 
     let func_name = func_name.into_pcstr();
@@ -55,4 +71,113 @@ pub fn find_func_addr(lib_name: &str, func_name: &str) -> Result<*const c_void, 
     };
 
     Ok(func_addr as *const c_void)
+}
+
+/// Take the original function prologue and put it in the trampoline
+///
+/// Transforms any RIP-relative address into an absolute address
+pub fn relocate_prologue_to_trampoline(
+    orig: *const u8,
+    trampoline: *mut u8,
+    stolen_len: usize,
+) -> Result<usize, HookError> {
+    let code = unsafe { std::slice::from_raw_parts(orig, stolen_len) };
+    let mut decoder = Decoder::with_ip(64, code, orig as u64, DecoderOptions::NONE);
+
+    let mut read = 0usize;
+    let mut instructions: Vec<Instruction> = Vec::new();
+
+    // For debugging
+    let print_instr = |instr: &Instruction, param: bool| { 
+        let mut formatter = NasmFormatter::new();
+        let mut output = String::new();
+        formatter.format(instr, &mut output);
+        if !param {
+            println!("  {output}");
+        } else {
+            println!("  ({output})");
+        }
+    };
+
+    while read < stolen_len {
+        let instr = decoder.decode();
+        read += instr.len();
+
+        if instr.flow_control() != FlowControl::Next {
+            panic!("Flow control instruction found in prologue. Bailing");
+        }
+
+        instructions.push(instr);
+    }
+
+    println!("Instructions");
+    let mut out: Vec<Instruction> = Vec::with_capacity(instructions.len() * 2);
+    for instr in instructions {
+        print_instr(&instr, false);
+
+        if instr.is_ip_rel_memory_operand() {
+            let abs = instr.ip_rel_memory_address();
+            let mov_r10_abs = Instruction::with2(Code::Mov_r64_imm64, Register::R10, abs)
+                .expect("Unable to build new instruction");
+
+            out.push(mov_r10_abs);
+
+            print_instr(&mov_r10_abs, true);
+        } else {
+            out.push(instr);
+        }
+    }
+
+    println!("Append absolute jump back to original function");
+    let jmp_r10 = Instruction::with1(Code::Jmp_rm64, Register::R10).expect("Unable to create jmp");
+
+    out.push(jmp_r10);
+
+    let encoder = BlockEncoder::encode(
+        64,
+        InstructionBlock::new(&out, trampoline as u64),
+        BlockEncoderOptions::NONE,
+    )
+    .expect("Unable to encode new buffer");
+
+    // unsafe {
+    //     std::ptr::copy_nonoverlapping(encoder.code_buffer.as_ptr(), trampoline, written);
+    // }
+
+    // let back = (orig as u64) + (stolen_len as u64);
+    // let p = unsafe { trampoline.add(written) };
+
+    // // mov rax, imm64
+    // unsafe { *p.add(0) = 0x48 };
+    // unsafe { *p.add(1) = 0xB8 };
+    // unsafe { *(p.add(2) as *mut u64) = back };
+
+    // // jmp rax
+    // unsafe { *p.add(10) = 0xFF };
+    // unsafe { *p.add(11) = 0xE0 };
+
+    // written += 12;
+
+    // let mut old: PAGE_PROTECTION_FLAGS = PAGE_PROTECTION_FLAGS(0u32);
+
+    // unsafe {
+    //     VirtualProtect(trampoline as _, written, PAGE_EXECUTE_READ, &mut old)
+    //         .expect("Unable to change memory protection level")
+    // };
+
+    // let _ = unsafe {
+    //     FlushInstructionCache(GetCurrentProcess(), Some(trampoline as _), written)
+    //         .expect("Unable to flush instruction cache")
+    // };
+
+    Ok(encoder.code_buffer.len())
+}
+
+
+fn steal_bytes(prologue: *const u8, num_of_bytes: usize) {
+    // This changes a *const u8 into a &[u8]
+    let code = unsafe { std::slice::from_raw_parts(prologue, num_of_bytes) };
+    let mut decoder = Decoder::with_ip(64, code, prologue as u64, DecoderOptions::NONE);
+
+    
 }
